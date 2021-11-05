d520c09037ba71420e491cfeec78786368a3bb90
nabil <merchant.nabil@gmail.com>
2021-11-04 20:27:01 -0500

    adding some use cases with pandas

--- main.py
+++ main.py
@@ -18,4 +18,32 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(type(data))
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# Get data in columns
+average = sum(temp_list) / len(temp_list)
+print(average)
+print(data["temp"].mean()) # same result but with less code
+print(data["temp"].max())
+
+print(data["condition"])
+print(data.condition) # works as the same as previous line
+
+
+# Get data in rows
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()]) # print day with max temperature
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
